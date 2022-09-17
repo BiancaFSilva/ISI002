0a1e2a72fe32ca0aa09350174c718dc4b2a80088
Bianca Silva <biancaflorianodasilva@gmail.com>
2022-09-16 21:06:47 -0300

    Jogo finalizado

--- Batch/Atividades/Adivinhacao.bat
+++ Batch/Atividades/Adivinhacao.bat
@@ -4,57 +4,82 @@ mode 60,30
 color 0f
 
 :inicio
-cls
-echo ============================================================
-echo                     Jogo da Adivinhacao
-echo ------------------------------------------------------------
-echo Sorteamos um numero de 1 a 100, sera que voce adivinha esse numero em 5 tentativas?
-echo ============================================================
-echo.
-set /p op= Deseja testar sua sorte? (S/N): 
-if /i %op% EQU s (goto:jogo)
-if /i %op% EQU n (exit)
-
-:jogo
-set /a num= (%random% %%100) + 1
-set /a vida= 5
-
-cls
-echo ============================================================
-echo                     Jogo da Adivinhacao
-echo ------------------------------------------------------------
-echo.
-echo Numero sorteado: %num%
-for /l %%n in (5,-1,0) do (    
-    echo Tentativa(s) restante(s): %vida%
-    set /p sorte= Seu palpite: 
+    cls
+    echo ============================================================
+    echo                     Jogo da Adivinhacao
+    echo ------------------------------------------------------------
+    echo.
+    echo Sorteamos um numero de 1 a 100, sera que voce adivinha esse numero em 5 tentativas?
     echo.
     echo ============================================================
     echo.
+    set /p op= Deseja testar sua sorte? (S/N): 
+    if /i %op% EQU s ( goto:sorteio )
+    if /i %op% EQU n ( exit )
 
-    if %sorte% EQU %num% (goto:vitoria)
-    if %sorte% LEQ %num% (
-        set /a vida= %vida% - 1
-        set /p sorte= Tente um numero MAIOR: 
+:sorteio
+    set /a num= (%random% %%100) + 1    
+    set /a vida= 5
+    goto:jogo
+
+:jogo
+    cls
+    echo ============================================================
+    echo                     Jogo da Adivinhacao
+    echo ------------------------------------------------------------
+    echo.
+    echo Digite um numero entre 1 e 100
+    echo Tentativa(s) restante(s): %vida%
+    echo Numero sorteado: %num%
+    echo.
+    echo ============================================================
+    echo.        
+    set /p sorte= Seu palpite: 
+
+    rem Validação da entrada de dados
+    if %sorte% GTR 100 (
+        echo. 
+        echo Digite um numero valido! 
     )
-    if %sorte% GEQ %num% (
-        set /a vida= %vida% - 1
-        set /p sorte= Tente um numero MENOR: 
+    if %sorte% LSS 1 ( 
+        echo.
+        echo Digite um numero valido! 
     )
-    if %vida% EQU 0 (goto:derrota)
-)
 
-:vitoria
-echo.
-echo VOCE VENCEU!
-echo.
-set /p resp= Deseja jogar novamente? (S/N): 
-if /i %resp% == s (goto:inicio) else if /i %resp == n (exit)
+    goto:verifica
 
-:derrota
-echo.
-echo VOCE PERDEU! 
-echo O numero sorteado era %num%
-echo.
-set /p resp= Deseja jogar novamente? (S/N): 
-if /i %resp% == s (goto:inicio) else if /i %resp == n (exit)
+:verifica
+    set /a erro = %vida% - 1
+
+    rem Compara o número sorteado com o número digitado
+    if %sorte% EQU %num% ( 
+        echo.
+        echo VOCE VENCEU!        
+
+        goto:novoJogo
+    )
+    if %sorte% LEQ %num% ( 
+        echo.         
+        set /p %sorte%= Tente um numero MAIOR: 
+    ) 
+    if %sorte% GEQ %num% ( 
+        echo.  
+        set /p %sorte%= Tente um numero MENOR: 
+    )                       
+    if %vida% EQU 0 (
+        echo.
+        echo VOCE PERDEU! 
+        echo O numero sorteado era %num%
+
+        goto:novoJogo
+    )
+
+    echo Tentativa(s) restante(s): %erro%
+    goto:verifica
+
+:novoJogo
+    echo.
+    set /p resp= Deseja jogar novamente? (S/N): 
+
+    if /i %resp% == s ( goto:inicio ) 
+    if /i %resp% == n ( exit )
--- Batch/Atividades/JoKenPo.bat
+++ Batch/Atividades/JoKenPo.bat
@@ -4,94 +4,133 @@ mode 60,30
 color 0f
 
 :inicio
-cls
-set /a ganha= 0
-set /a perde= 0
-set /a empata= 0
-echo ============================================================
-echo                          JO KEN PO
-echo ------------------------------------------------------------
-echo [1] Novo Jogo
-echo [2] Regras
-echo [x] Sair
-echo ============================================================
-echo.
-set /p nome= Informe seu nome: 
-set /p op= Digite a opcao desejada: 
-if %op% EQU 1 (goto:jogo)
-if %op% EQU 2 (goto:regras)
-if /i %op% EQU x (exit) else (
-    echo Opcao invalida...
-    pause
-    goto:inicio
-)
+    cls
+
+    set /a ganha= 0
+    set /a perde= 0
+    set /a empata= 0
+
+    echo ============================================================
+    echo                          JO KEN PO
+    echo ------------------------------------------------------------
+    echo.
+    echo [1] Novo Jogo
+    echo [2] Regras
+    echo [x] Sair
+    echo.
+    echo ============================================================
+    echo.
+
+    set /p nome= Informe seu nome: 
+    set /p op= Digite a opcao desejada: 
+
+    if %op% EQU 1 ( goto:jogo )
+    if %op% EQU 2 ( goto:regras )
+    if /i %op% EQU x ( exit ) else (
+        echo Opcao invalida...
+
+        pause > nul
+        goto:inicio
+    )
 
 :regras
-cls
-echo ============================================================
-echo                            REGRAS
-echo ------------------------------------------------------------
-echo PEDRA: ganha de tesoura e lagarto, perde para Spock e papel,empata com pedra
-echo.
-echo PAPEL: ganha de pedra e Spock, perde para tesoura e lagarto,empata com papel
-echo.
-echo TESOURA: ganha de papel e lagarto, perde para pedra e Spock,empata com tesoura
-echo.
-echo LAGARTO: ganha de papel e Spock, perde para pedra e tesoura,empata com lagarto
-echo.
-echo SPOCK: ganha de pedra e tesoura, perde para lagarto e papel,empata com Spock
-echo ============================================================
-echo.
+    cls
+    echo ============================================================
+    echo                            REGRAS
+    echo ------------------------------------------------------------
+    echo.
+    echo PEDRA: ganha de tesoura e lagarto, perde para Spock e papel,empata com pedra
+    echo.
+    echo PAPEL: ganha de pedra e Spock, perde para tesoura e lagarto,empata com papel
+    echo.
+    echo TESOURA: ganha de papel e lagarto, perde para pedra e Spock,empata com tesoura
+    echo.
+    echo LAGARTO: ganha de papel e Spock, perde para pedra e tesoura,empata com lagarto
+    echo.
+    echo SPOCK: ganha de pedra e tesoura, perde para lagarto e papel,empata com Spock
+    echo.
+    echo ============================================================
+    echo.
 
-pause
-goto:inicio
+    pause > nul
+    goto:inicio
 
 :jogo
-cls
-set /a sort= (%random% %%5) + 1
-cls
-echo ============================================================
-echo                          JO KEN PO
-echo ------------------------------------------------------------
-echo Bem-vindo(a) %nome%, essas sao suas opcoes:
-echo.
-echo [1] Pedra
-echo [2] Papel
-echo [3] Tesoura
-echo [4] Lagarto
-echo [5] Spock
-echo [r] Regras 
-echo [x] Sair
-echo.
-set /p op= Digite a opcao desejada: 
-echo ------------------------------------------------------------
-echo.
+    cls
+    rem Sorteia a jogada da máquina
+    set /a sort= (%random% %%5) + 1
 
-if /i %op% EQU r (goto:regras)
-if /i %op% EQU x (exit) else (
-    echo Opcao invalida...
-    pause
-)
+    echo ============================================================
+    echo                          JO KEN PO
+    echo ------------------------------------------------------------
+    echo.
+    echo Bem-vindo(a) %nome%, essas sao suas opcoes:
+    echo.
+    echo          [1] Pedra      [2] Papel      [3] Tesoura
+    echo          [4] Lagarto    [5] Spock      [x] Sair
+    echo.
+    set /p op= Digite a opcao desejada: 
+    echo.
+    echo ============================================================
+    echo.
+    if /i %op% EQU x ( exit ) 
 
-if %op% EQU 1 ( echo %nome% escolheu PEDRA )
-if %op% EQU 2 ( echo %nome% escolheu PAPEL )
-if %op% EQU 3 ( echo %nome% escolheu TESOURA )
-if %op% EQU 4 ( echo %nome% escolheu LAGARTO )
-if %op% EQU 5 ( echo %nome% escolheu SPOCK )
+    rem Mostra a jogada 
+    if %op% EQU 1 ( echo %nome% escolheu PEDRA )
+    if %op% EQU 2 ( echo %nome% escolheu PAPEL )
+    if %op% EQU 3 ( echo %nome% escolheu TESOURA )
+    if %op% EQU 4 ( echo %nome% escolheu LAGARTO )
+    if %op% EQU 5 ( echo %nome% escolheu SPOCK )    
 
-if %sort% EQU 1 ( echo Computador escolheu PEDRA )
-if %sort% EQU 2 ( echo Computador escolheu PAPEL )
-if %sort% EQU 3 ( echo Computador escolheu TESOURA )
-if %sort% EQU 4 ( echo Computador escolheu LAGARTO )
-if %sort% EQU 5 ( echo Computador escolheu SPOCK )
+    rem Mostra a jogada da máquina
+    if %sort% EQU 1 ( echo Computador escolheu PEDRA )
+    if %sort% EQU 2 ( echo Computador escolheu PAPEL )
+    if %sort% EQU 3 ( echo Computador escolheu TESOURA )
+    if %sort% EQU 4 ( echo Computador escolheu LAGARTO )
+    if %sort% EQU 5 ( echo Computador escolheu SPOCK )
 
-if %op% EQU %sort% ( set /a empata= %empata% + 1 )
+    rem Compara as jogas e define se houve vitório, derrota ou empate
+    if %op% EQU %sort% ( set /a empata= %empata% + 1 )
 
-echo.
-echo ------------------------------------------------------------
-echo   Vitoria(s): %ganha%         Empate(s): %empata%         Derrota(s): %perde%
-echo ============================================================
-echo.
-set /p op= Deseja jogar novamente? (S/N): 
-if /i %op% EQU s (goto:jogo)
-if /i %op% EQU n (exit)
+    if %op% EQU 1 (
+        if %sort% EQU 3 ( set /a ganha= %ganha% + 1 )
+        if %sort% EQU 4 ( set /a ganha= %ganha% + 1 )
+        if %sort% EQU 2 ( set /a perde= %perde% + 1 )
+        if %sort% EQU 5 ( set /a perde= %perde% + 1 )
+    )   
+    if %op% EQU 2 (
+        if %sort% EQU 1 ( set /a ganha= %ganha% + 1 )
+        if %sort% EQU 5 ( set /a ganha= %ganha% + 1 )
+        if %sort% EQU 3 ( set /a perde= %perde% + 1 )
+        if %sort% EQU 4 ( set /a perde= %perde% + 1 )
+    ) 
+    if %op% EQU 3 (
+        if %sort% EQU 2 ( set /a ganha= %ganha% + 1 )
+        if %sort% EQU 4 ( set /a ganha= %ganha% + 1 )
+        if %sort% EQU 1 ( set /a perde= %perde% + 1 )
+        if %sort% EQU 5 ( set /a perde= %perde% + 1 )
+    ) 
+    if %op% EQU 4 (
+        if %sort% EQU 2 ( set /a ganha= %ganha% + 1 )
+        if %sort% EQU 5 ( set /a ganha= %ganha% + 1 )
+        if %sort% EQU 1 ( set /a perde= %perde% + 1 )
+        if %sort% EQU 3 ( set /a perde= %perde% + 1 )
+    ) 
+    if %op% EQU 5 (
+        if %sort% EQU 1 ( set /a ganha= %ganha% + 1 )
+        if %sort% EQU 3 ( set /a ganha= %ganha% + 1 )
+        if %sort% EQU 2 ( set /a perde= %perde% + 1 )
+        if %sort% EQU 4 ( set /a perde= %perde% + 1 )
+    ) 
+
+    echo.
+    echo ------------------------------------------------------------
+    echo.
+    echo   Vitoria(s): %ganha%         Empate(s): %empata%         Derrota(s): %perde%
+    echo.
+    echo ============================================================
+    echo.
+
+    set /p op= Deseja jogar novamente? (S/N): 
+    if /i %op% EQU s ( goto:jogo )
+    if /i %op% EQU n ( exit )
